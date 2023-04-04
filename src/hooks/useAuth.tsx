673ddf27acf006dae447586ee42e807451ef8c33
Daniela-Malahova <malahova.daniela@gmail.com>
2023-04-04 10:09:09 +0300

    fixed redirect to auth page

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,4 +1,4 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { useAppSelector } from "./reduxHooks";
 
 interface CheckAuthProps {
@@ -7,9 +7,12 @@ interface CheckAuthProps {
 
 const UseAuth = ({ children }: CheckAuthProps) => {
   const { isAuth } = useAppSelector((state) => state.user);
+  const { pathname } = useLocation();
 
   if (isAuth) {
     return <Navigate to="/books" />;
+  } else if (pathname === "/") {
+    return <Navigate to="auth" />;
   }
 
   return children;
